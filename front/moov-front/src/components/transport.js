289e5d7ac722aef780676ef6b69e4845496d1dd6
Rem-G <gosselin.rem@gmail.com>
2021-05-01 11:23:35 +0200

    init map

--- front/moov-front/src/components/transport.js
+++ front/moov-front/src/components/transport.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import SearchBar from './searchbar';
+import Map from './map';
 import "../style/transport.css";
+import "../style/map.css";
 
 export default function Transport({station, updateStation, searchReq, updateSearchReq}){
 
@@ -12,6 +14,9 @@ export default function Transport({station, updateStation, searchReq, updateSear
 	return(
 		<div >
 			<SearchBar searchReq={searchReq} updateSearchReq={updateSearchReq}/>
+			<div className="map r1 c1">
+				<Map/>
+			</div>
 		</div>
 	);
 };
